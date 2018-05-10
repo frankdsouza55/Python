71330ef3ebfb4abf0234210fed57e470ce3c74e4
Frank D'Souza <frankdsouza55@gmail.com>
2018-05-10 18:26:03 +0530

    Small update!
    Reduced LOC

--- Pyramid pattern.py
+++ Pyramid pattern.py
@@ -1,7 +1,5 @@
 # Pyramid pattern
     
-n = int(input("Enter number of lines : "))
-for i in range(0,n+1):
-    for j in range(0,i):
-        print '*',
-    print "\n"
+n = input("Enter the number of lines: ")
+for i in range(1,n+1):
+        print "*" * i
